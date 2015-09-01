ad173a8ba5d557fbbaaef629a6d02a9fd2503fa5
biruzka <biruzka@gmail.com>
2015-09-01 14:36:31 +0300

    not important

--- app/Infrastructure/helpers.js
+++ app/Infrastructure/helpers.js
@@ -39,3 +39,35 @@ function makeId(ListOfData) {
 }
 
 
+var BuyingService = new BuyingService();
+
+var Service = {
+
+    ProductstoOrderEnough: function (order, product) {
+        return (product.get("count") -  order.get("count") >= 0);
+    },
+
+    putProducttoShop: function (productEn,shopEn){
+        productEn.bindShop(shopEn);
+        ProductRepository.update(productEn);
+    },
+
+    getAllProductsOfShop: function (shopEn) {
+        var arr = [];
+        arr = productRepository.search(owner,shopEn.getId());
+        return arr;
+    },
+
+    buy: function (order) {
+        try {
+            BuyingService.buy(order); //сущность
+        }
+        catch (e) {
+            console.log(e);
+        }
+
+    }
+
+}
+
+
